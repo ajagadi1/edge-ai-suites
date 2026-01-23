142b20506a691584412d249a97042520af791ce6
Ashish Jagadish <ashish.jagadish@intel.com>
2026-01-23 09:56:51 +0530

    Revert "updated helm chart"
    This reverts commit e641619132e16430771cc227621727b40db7c80c.

--- metro-ai-suite/metro-vision-ai-app-recipe/smart-intersection/chart/files/webserver/download_database.sh
+++ metro-ai-suite/metro-vision-ai-app-recipe/smart-intersection/chart/files/webserver/download_database.sh
@@ -6,11 +6,7 @@ else
   echo "File does NOT exist: /data/smart-intersection-ri.tar.bz2"
   echo "Downloading file from GitHub..."
   apk add --no-cache wget
-  {{- if eq .Values.gitSource.type "tag" }}
-  wget -O /data/smart-intersection-ri.tar.bz2 "https://github.com/open-edge-platform/edge-ai-suites/raw/refs/tags/{{ .Values.gitSource.ref }}/metro-ai-suite/metro-vision-ai-app-recipe/smart-intersection/src/webserver/smart-intersection-ri.tar.bz2"
-  {{- else }}
-  wget -O /data/smart-intersection-ri.tar.bz2 "https://github.com/open-edge-platform/edge-ai-suites/raw/refs/heads/{{ .Values.gitSource.ref }}/metro-ai-suite/metro-vision-ai-app-recipe/smart-intersection/src/webserver/smart-intersection-ri.tar.bz2"
-  {{- end }}
+  wget -O /data/smart-intersection-ri.tar.bz2 "{{ .Values.externalUrls.githubRepo }}/raw/refs/tags/{{ .Values.version.release }}/metro-ai-suite/metro-vision-ai-app-recipe/smart-intersection/src/webserver/smart-intersection-ri.tar.bz2"
   if [ $? -eq 0 ]; then
     echo "File downloaded successfully to /data/smart-intersection-ri.tar.bz2"
   else
--- metro-ai-suite/metro-vision-ai-app-recipe/smart-intersection/chart/templates/dlstreamer-pipeline-server/scripts/init-models.sh
+++ metro-ai-suite/metro-vision-ai-app-recipe/smart-intersection/chart/templates/dlstreamer-pipeline-server/scripts/init-models.sh
@@ -9,17 +9,10 @@ else
   echo "Downloading models from GitHub..."
   apk add --no-cache wget tar
   cd /tmp
-  {{- if eq .Values.gitSource.type "tag" }}
-  wget -O models.tar.gz https://github.com/open-edge-platform/edge-ai-suites/archive/refs/tags/{{ .Values.gitSource.ref }}.tar.gz
+  wget -O models.tar.gz {{ .Values.externalUrls.githubRepo }}/archive/refs/tags/{{ .Values.version.modelsRelease }}.tar.gz
   tar -xzf models.tar.gz
-  EXTRACTED_DIR="edge-ai-suites-{{ .Values.gitSource.ref | trimPrefix "v" }}"
-  {{- else }}
-  wget -O models.tar.gz https://github.com/open-edge-platform/edge-ai-suites/archive/refs/heads/{{ .Values.gitSource.ref }}.tar.gz
-  tar -xzf models.tar.gz
-  EXTRACTED_DIR="edge-ai-suites-{{ .Values.gitSource.ref | replace "/" "-" }}"
-  {{- end }}
   mkdir -p /data/models
-  cp -r ${EXTRACTED_DIR}/metro-ai-suite/smart-intersection/src/dlstreamer-pipeline-server/models/* /data/models/
+  cp -r edge-ai-suites-{{ .Values.version.modelsRelease | replace "v" "" }}/metro-ai-suite/smart-intersection/src/dlstreamer-pipeline-server/models/* /data/models/
   echo "Models downloaded successfully"
   touch /data/models/.done
 fi
